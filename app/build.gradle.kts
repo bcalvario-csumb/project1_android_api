9b6de3e30dc70c36caa97d2da25a179c66bc828b
kalvarioz <dinagreen32@gmail.com>
2026-08-29 19:49:17 -0700

    Compose features fully implemented/repaired. Application now compiles and runs (tested on medium phone, API 34, x86_64/ARM64)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -22,7 +22,7 @@ android {
     buildTypes {
         release {
             optimization {
-                enable = false
+                enable = true
             }
         }
     }
