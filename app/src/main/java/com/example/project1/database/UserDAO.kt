c87ae674a0d78f7aadb157d991723c96adfcaf68
aalex <aletrujillo@csumb.edu>
2026-09-03 14:40:23 -0700

    The version to which gradle works and likes the database. Updated the user DAO to be good with login.

--- app/src/main/java/com/example/project1/database/UserDAO.kt
+++ app/src/main/java/com/example/project1/database/UserDAO.kt
@@ -9,6 +9,9 @@ interface UserDAO {
    @Insert
    suspend fun insertUser(user: User)
 
-   @Query("SELECT * FROM users WHERE username = :username LIMIT 1")
-   suspend fun getUserByUsername(username: String): User?
+   @Query("SELECT * FROM users WHERE email = :email LIMIT 1")
+   suspend fun getUserByEmail(email: String): User?
+
+   @Query("SELECT * FROM users WHERE email = :email AND password = :password LIMIT 1")
+   suspend fun validateLogin(email: String, password: String): User?
 }
