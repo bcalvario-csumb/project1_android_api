efa108afbc8ae4ed3da82b498aede5e34008525d
aalex <aletrujillo@csumb.edu>
2026-09-03 21:36:57 -0700

    Did the Card and userHasCard DAOs, as well as the UserHasCard entity.

---
+++ app/src/main/java/com/example/project1/database/CardDAO.kt
@@ -0,0 +1,14 @@
+package com.example.project1.database
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.project1.database.entities.Card
+
+@Dao
+interface CardDAO {
+    @Insert
+    suspend fun insertCard(card: Card)
+
+    @Query("SELECT * FROM cards")
+    suspend fun getAllCards(): List<Card>
+}
