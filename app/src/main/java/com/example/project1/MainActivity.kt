c320d32bb9d8c71ee5ada9e1c1772aa32d6417bb
aalex <aletrujillo@csumb.edu>
2026-09-07 16:45:00 -0700

    Pushed in a database variable inside main activity.

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -6,10 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.project1.ui.theme.MyApplicationTheme
 import com.example.project1.ui.nav.AppNavHost
+import androidx.room.Room
+import com.example.project1.database.GameDatabase
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val database = Room.databaseBuilder(applicationContext, GameDatabase::class.java, "game_database").build()
+
         setContent {
             MyApplicationTheme {
                 AppNavHost();
--- app/src/main/java/com/example/project1/database/entities/User.kt
+++ app/src/main/java/com/example/project1/database/entities/User.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 data class User (
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
-    val name: String,
     val email: String,
-    val password: String
+    val password: String,
+    val name: String
 )
