94f7ecbc8d78640f76e2388cd1cb9a60f6544156
Solian21 <soliancarlos@gmail.com>
2026-09-13 12:28:22 -0700

    Finished Adding in ErrorProne

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,13 @@
+import net.ltgt.gradle.errorprone.errorprone
+import org.gradle.api.tasks.compile.JavaCompile
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.androidx.room)
     alias(libs.plugins.kotlin.serialization)
+    id("net.ltgt.errorprone")
 }
 
 android {
@@ -73,5 +77,27 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
 
+    errorprone("com.google.errorprone:error_prone_core:2.31.0")
 
+}
+
+/**
+ * The ErrorProne code was vibecoded using AI - Carlos
+ *
+ * it is going through Androids configurations that end in AnnotationProcessor
+ * All these configurations ending in AnnotationProcessor are what Andrioid uses to complile
+ * We are then saying to inherit the Error Prone method
+ *
+ * in the second line we are saying to find every task that compiles java and then
+ * enable error prone. We are also saying to disable warnings generated from source files.
+ * Finally we have our if conditon which is asking if we are compiling tests
+ */
+configurations.matching { it.name.endsWith("AnnotationProcessor") }.configureEach { extendsFrom(configurations.errorprone.get()) }
+tasks.withType<JavaCompile>().configureEach {
+    options.errorprone.enabled = true
+    options.errorprone.disableWarningsInGeneratedCode = true
+
+    if (name.contains("Test", ignoreCase = true)) {
+        options.errorprone.compilingTestOnlyCode = true
+    }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.androidx.room) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    id("net.ltgt.errorprone") version "5.1.1" apply false
 }
